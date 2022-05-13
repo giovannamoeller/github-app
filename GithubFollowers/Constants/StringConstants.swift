d648a1e10ee08815e54f9449ac9f59ba9fc079cd
Giovanna <giovannagmoeller@gmail.com>
2022-05-13 17:17:03 -0300

    adding string constants as extensions to prevent mistakes

--- GithubFollowers/Constants/StringConstants.swift
+++ GithubFollowers/Constants/StringConstants.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+extension String {
+  
+  // MARK: - Images
+  static let logoImage = "gh-logo"
+  static let searchIcon = "magnifyingglass"
+  static let favoriteIcon = "heart"
+  static let favoriteIconHighlighted = "heart.fill"
+  
+  // MARK: - Colors
+  static let backgroundColor = "backgroundColor"
+  static let primaryColor = "primaryColor"
+}
