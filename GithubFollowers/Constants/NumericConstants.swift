5bdf11b0df10150a244c3cefe20cbc54fb14cbd2
Giovanna <giovannagmoeller@gmail.com>
2022-05-13 19:11:34 -0300

    adding numeric layout constants

---
+++ GithubFollowers/Constants/NumericConstants.swift
@@ -0,0 +1,15 @@
+//
+//  NumericConstants.swift
+//  GithubFollowers
+//
+//  Created by Giovanna Moeller on 13/05/22.
+//
+
+import Foundation
+
+struct NumericConstants {
+  
+  // MARK: - FontSize
+  let fontSize = 14.0
+  let buttonCornerRadius = 12.0
+}
