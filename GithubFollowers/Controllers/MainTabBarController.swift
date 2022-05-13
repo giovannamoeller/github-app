8fb20d9d1e538a67435523b7eee283b23581f7bd
Giovanna <giovannagmoeller@gmail.com>
2022-05-13 19:12:50 -0300

    using constants on tab bar

--- GithubFollowers/Controllers/MainTabBarController.swift
+++ GithubFollowers/Controllers/MainTabBarController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
   
+  private lazy var kStr = StringConstants()
+  private lazy var kNum = NumericConstants()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     configureTabBar()
@@ -18,20 +21,16 @@ class MainTabBarController: UITabBarController {
   private func configureTabBar() {
     tabBar.tintColor = .black
     viewControllers = [
-      embedInNavigationController(SearchViewController(), title: "Search", icon: String.searchIcon, iconHighlighted: nil),
-      embedInNavigationController(SearchViewController(), title: "Favorites", icon: String.favoriteIcon, iconHighlighted: String.favoriteIconHighlighted),
+      embedInNavigationController(SearchViewController(), title: "Search", icon: kStr.searchIcon, iconHighlighted: nil),
+      embedInNavigationController(SearchViewController(), title: "Favorites", icon: kStr.favoriteIcon, iconHighlighted: kStr.favoriteIconHighlighted),
     ]
   }
   
   private func embedInNavigationController(_ vc: UIViewController, title: String, icon: String, iconHighlighted: String?) -> UINavigationController {
-    
     let navVC = UINavigationController(rootViewController: vc)
     let selectedImageView = iconHighlighted == nil ? UIImage(systemName: icon, withConfiguration: UIImage.SymbolConfiguration(weight: .bold))! : UIImage(systemName: iconHighlighted!)!
-    
     navVC.tabBarItem = UITabBarItem(title: title, image: UIImage(systemName: icon), selectedImage: selectedImageView)
-    
     return navVC
-    
   }
   
 }
