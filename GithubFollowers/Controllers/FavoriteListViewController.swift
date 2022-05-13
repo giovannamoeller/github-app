7b556fc75f3ce8ca84b0b4166b7cd98f550ebd31
Giovanna <giovannagmoeller@gmail.com>
2022-05-13 19:32:11 -0300

    favorite list controller title

--- GithubFollowers/Controllers/FavoriteListViewController.swift
+++ GithubFollowers/Controllers/FavoriteListViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 class FavoriteListViewController: UIViewController {
   
   private lazy var favoriteListView = FavoriteListView()
+  private lazy var kStr = StringConstants()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    configureUI()
+  }
+  
+  private func configureUI() {
     view = favoriteListView
+    title = kStr.favoritesTabBarText
+    navigationController?.navigationBar.prefersLargeTitles = true
   }
   
 }
