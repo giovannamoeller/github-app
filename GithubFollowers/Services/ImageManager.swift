bc868dc4f6963072e2797a429050ba327ab3daf7
Giovanna <giovannagmoeller@gmail.com>
2022-05-20 20:13:33 -0300

    implementing cache image saving

--- GithubFollowers/Services/ImageManager.swift
+++ GithubFollowers/Services/ImageManager.swift
@@ -11,12 +11,18 @@ import UIKit
 class ImageManager {
   
   static let shared = ImageManager()
+  private let cache = NSCache<NSString, UIImage>()
   private init() {}
   
   func downloadImage(from urlString: String, completionHandler: @escaping (UIImage) -> ()) {
+    if let image = checkIfImageIsAlreadyInCache(urlString: urlString) {
+      completionHandler(image)
+      return
+    }
     guard let url = URL(string: urlString) else { return }
     
-    let task = URLSession.shared.dataTask(with: url) { data, response, error in
+    let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+      guard let self = self else { return }
       if error != nil { return }
       guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
         return
@@ -25,8 +31,20 @@ class ImageManager {
         return
       }
       guard let image = UIImage(data: data) else { return }
+      self.setImageToCache(image: image, urlString: urlString)
       completionHandler(image)
     }
     task.resume()
+  }
+  
+  func checkIfImageIsAlreadyInCache(urlString: String) -> UIImage? {
+    let cacheKey = NSString(string: urlString)
+    guard let image = cache.object(forKey: cacheKey) else { return nil }
+    return image
+  }
+  
+  func setImageToCache(image: UIImage, urlString: String) {
+    let cacheKey = NSString(string: urlString)
+    cache.setObject(image, forKey: cacheKey)
   }
 }
