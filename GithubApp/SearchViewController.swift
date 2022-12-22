4141fed056538129342f099990bdf24c6fc98cf4
Giovanna <giovannagmoeller@gmail.com>
2022-12-22 20:53:50 -0300

    deleting storyboard and implementing view code

--- GithubApp/SearchViewController.swift
+++ GithubApp/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GithubApp
 //
 //  Created by Giovanna Moeller on 22/12/22.
@@ -7,10 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
 
