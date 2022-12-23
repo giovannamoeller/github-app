af12f5916b43753a879be0a3533bb58e4246cb06
Giovanna <giovannagmoeller@gmail.com>
2022-12-23 16:52:44 -0300

    changing logo image and creating text field view

--- GithubApp/SearchViewController.swift
+++ GithubApp/SearchViewController.swift
@@ -8,15 +8,40 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    private lazy var logoImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "gh-logo"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var textField = GFTextField("Enter Username")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
+        setConstraints()
         // Do any additional setup after loading the view.
     }
     
     private func setLayout() {
-        
+        view.backgroundColor = .white
+        view.addSubview(logoImageView)
+        view.addSubview(textField)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 128),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 190.0),
+            logoImageView.heightAnchor.constraint(equalToConstant: 190.0),
+            
+            textField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 96.0),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 64),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -64),
+            textField.heightAnchor.constraint(equalToConstant: 64.0)
+        ])
     }
 
 
---
+++ GithubApp/Views/GFTextField.swift
@@ -0,0 +1,28 @@
+//
+//  GFTextField.swift
+//  GithubApp
+//
+//  Created by Giovanna Moeller on 23/12/22.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    init(_ text: String) {
+        super.init(frame: .zero)
+        placeholder = text
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setLayout() {
+        translatesAutoresizingMaskIntoConstraints = false
+        borderStyle = .roundedRect
+        
+    }
+
+}
