9a2bea8859dd78dea2c6f9235cc89345edc2e969
Giovanna <giovannagmoeller@gmail.com>
2022-12-23 21:46:51 -0300

    creating an extension with colors

---
+++ GithubApp/Extensions/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  GithubApp
+//
+//  Created by Giovanna Moeller on 23/12/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static let mainColor = UIColor(red: 45/255, green: 178/255, blue: 186/255, alpha: 1.0)
+    static let errorColor = UIColor(red: 220/255, green: 38/255, blue: 62/255, alpha: 1.0)
+}
---
+++ GithubApp/Views/GFButton.swift
@@ -0,0 +1,20 @@
+//
+//  GFButton.swift
+//  GithubApp
+//
+//  Created by Giovanna Moeller on 23/12/22.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
