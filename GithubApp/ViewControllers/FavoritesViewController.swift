5d3126bafc45771df369b746abe61e127f087eb8
Giovanna <giovannagmoeller@gmail.com>
2022-12-30 23:36:27 -0300

    implementing tab bar controller

---
+++ GithubApp/ViewControllers/FavoritesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesViewController.swift
+//  GithubApp
+//
+//  Created by Giovanna Moeller on 30/12/22.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
