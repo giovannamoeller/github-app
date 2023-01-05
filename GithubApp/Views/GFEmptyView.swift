5888c49a898c3bb6a9d7e0a767a5d767ecb78deb
Giovanna <giovannagmoeller@gmail.com>
2023-01-04 22:47:41 -0300

    creating a method to show empty state view on view controller extension

---
+++ GithubApp/Views/GFEmptyView.swift
@@ -0,0 +1,20 @@
+//
+//  GFEmptyView.swift
+//  GithubApp
+//
+//  Created by Giovanna Moeller on 04/01/23.
+//
+
+import UIKit
+
+class GFEmptyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
