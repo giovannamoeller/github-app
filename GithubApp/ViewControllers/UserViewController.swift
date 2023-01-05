b4e4997606cc4608accfa4ab040897c37d560220
Giovanna <giovannagmoeller@gmail.com>
2023-01-05 14:33:56 -0300

    implementing stack view with informations and bio

--- GithubApp/ViewControllers/UserViewController.swift
+++ GithubApp/ViewControllers/UserViewController.swift
@@ -8,6 +8,68 @@
 import UIKit
 
 class UserViewController: UIViewController {
+    
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 16
+        stackView.alignment = .center
+        [avatarImageView, textStackView].forEach { view in
+            stackView.addArrangedSubview(view)
+        }
+        return stackView
+    }()
+    
+    private lazy var textStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 12
+        [usernameTitleLabel, nameLabel, locationLabel].forEach { view in
+            stackView.addArrangedSubview(view)
+        }
+        return stackView
+    }()
+    
+    private lazy var usernameTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var locationLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var bioLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var avatarImageView: GFAvatarImageView = {
+        let imgView = GFAvatarImageView()
+        return imgView
+    }()
         
     init(follower: Follower) {
         super.init(nibName: nil, bundle: nil)
@@ -25,17 +87,31 @@ class UserViewController: UIViewController {
     }
     
     private func setLayout() {
+        view.addSubview(mainStackView)
+        view.addSubview(bioLabel)
         view.backgroundColor = .systemBackground
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(dismissModal))
     }
     
     private func setConstraints() {
-        
+        NSLayoutConstraint.activate([
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            avatarImageView.heightAnchor.constraint(equalToConstant: 92),
+            
+            bioLabel.topAnchor.constraint(equalTo: mainStackView.bottomAnchor, constant: 32),
+            bioLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            bioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+        ])
     }
     
     private func getUser(_ username: String) {
+        showLoadingView()
         Network.shared.getUser(for: username) { [weak self] result in
             guard let self = self else { return }
+            self.dismissLoadingView()
             switch result {
             case .success(let user):
                 DispatchQueue.main.async {
@@ -50,6 +126,11 @@ class UserViewController: UIViewController {
     
     private func displayUserInfo(_ user: User) {
         print(user)
+        avatarImageView.downloadImage(from: user.avatarUrl)
+        usernameTitleLabel.text = user.username
+        nameLabel.text = user.name ?? "Name not informed"
+        locationLabel.text = user.location ?? "Location not informed"
+        bioLabel.text = user.bio ?? "Bio not informed"
     }
     
     @objc private func dismissModal() {
