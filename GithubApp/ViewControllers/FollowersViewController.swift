a7b240d3e6825a7673eb8b91e704ce63be8a0b06
Giovanna <giovannagmoeller@gmail.com>
2023-01-04 21:45:32 -0300

    implementing loading view (show and dismiss) in view controller

--- GithubApp/ViewControllers/FollowersViewController.swift
+++ GithubApp/ViewControllers/FollowersViewController.swift
@@ -67,8 +67,10 @@ class FollowersViewController: UIViewController {
     }
     
     private func getFollowers() {
+        showLoadingView()
         Network.shared.getFollowers(for: username, page: currentPage) { [weak self] result in
             guard let self = self else { return }
+            self.stopLoadingView()
             switch result {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
