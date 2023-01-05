91297a1f51edabf2dd3043625051d169855d20f4
Giovanna <giovannagmoeller@gmail.com>
2023-01-04 22:48:06 -0300

    creating a component for empty view'

--- GithubApp/Views/GFEmptyView.swift
+++ GithubApp/Views/GFEmptyView.swift
@@ -9,12 +9,51 @@ import UIKit
 
 class GFEmptyView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 28)
+        label.textColor = .secondaryLabel
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imgView = UIImageView(image: UIImage(named: "empty-state-logo"))
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        return imgView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+        setConstraints()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setMessage(_ message: String) {
+        messageLabel.text = message
+    }
+    
+    private func setLayout() {
+        addSubview(messageLabel)
+        addSubview(imageView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32),
+            
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 100),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 180),
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        ])
+    }
 
 }
