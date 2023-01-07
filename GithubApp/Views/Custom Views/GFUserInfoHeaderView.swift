5d79f323644724c948010b1f82c016e16c2878ca
Giovanna <giovannagmoeller@gmail.com>
2023-01-07 00:01:02 -0300

    organizing folders

---
+++ GithubApp/Views/Custom Views/GFUserInfoHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  GFUserInfoHeaderView.swift
+//  GithubApp
+//
+//  Created by Giovanna Moeller on 05/01/23.
+//
+
+import UIKit
+
+class GFUserInfoHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
